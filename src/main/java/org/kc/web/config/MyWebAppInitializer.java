3a6067eb2d239b3476bd023dc00f15e63586c613
Keshav C Chaure <keshav.chaure@yash.com>
2018-07-31 11:00:38 +0530

    first commit-simple spring mvc

---
+++ src/main/java/org/kc/web/config/MyWebAppInitializer.java
@@ -0,0 +1,26 @@
+package org.kc.web.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+
+public class MyWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// this is to configure web.xml
+		return new Class[] {WebConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[]{"/"};
+	}
+
+}
---
+++ src/main/java/org/kc/web/config/RootConfig.java
@@ -0,0 +1,8 @@
+package org.kc.web.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RootConfig {
+
+}
